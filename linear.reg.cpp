b3b6c189894b00dd2b3414a0227de2e569963c84
Kabir Singh <sahai.kabir1@gmail.com>
2021-12-28 20:24:51 -0700

    brian stop judging my naming

--- linear.reg.cpp
+++ linear.reg.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
+class holder{
+  public:
+    int vals[25] = {};
+    void executeEquation(int t1[], int t1x[], int extent){
+      for(int i = 0; i<extent; i++){
+        int tempval = 0;
+        tempval = t1[i] + t1x[i];
+        vals[i] = tempval;
+      }
+    }
+};
+
 int main(){
-  string equations_t1[50] = {};
-  string equations_t1x[50] = {};
+  int equations_t1[50] = {};
+  int equations_t1x[50] = {};
   int counter = 0;
   for(int i=0; i<5; i++){
     for(int j=0; j<5; j++){
       counter++;
-      equations_t1[counter] = to_string(i);
-      equations_t1x[counter] = to_string(j);
+      equations_t1[counter] = i;
+      equations_t1x[counter] = j;
     }
   }
+
+  holder holder;
+  holder.executeEquation(equations_t1, equations_t1x, 25);
+
   for(int i=0; i<25; i++){
-    cout << equations_t1[i+1] + ", ";
-    cout << equations_t1x[i+1] + "\n";
+    cout << to_string(equations_t1[i]) + ", ";
+    cout << to_string(equations_t1x[i]) + "\n";
+		cout << "value" << holder.vals[i] << "\n";
   }
   return 0;
 }
