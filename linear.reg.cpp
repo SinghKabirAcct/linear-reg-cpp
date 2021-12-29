60536d643439537354be6760d86b6b98db3075e3
Kabir Singh <sahai.kabir1@gmail.com>
2021-12-28 23:32:05 -0700

    gtg sleep

--- linear.reg.cpp
+++ linear.reg.cpp
@@ -4,36 +4,51 @@
 using namespace std;
 
 class holder{
+
   public:
+
     int vals[25] = {};
+
     void executeEquation(int t1[], int t1x[], int extent){
+
       for(int i = 0; i<extent; i++){
-        int tempval = 0;
-        tempval = t1[i] + t1x[i];
-        vals[i] = tempval;
+        for(int j = 0; j<5; j++){
+          int tempval = 0;
+          tempval = t1[i] + t1x[i]*j;
+          vals[(i*5)+j] = tempval;
+        }
       }
+
     }
+
 };
 
 int main(){
+
   int equations_t1[50] = {};
   int equations_t1x[50] = {};
   int counter = 0;
+
   for(int i=0; i<5; i++){
     for(int j=0; j<5; j++){
-      counter++;
       equations_t1[counter] = i;
       equations_t1x[counter] = j;
+			counter++;
     }
   }
-
-  holder holder;
-  holder.executeEquation(equations_t1, equations_t1x, 25);
 
   for(int i=0; i<25; i++){
     cout << to_string(equations_t1[i]) + ", ";
     cout << to_string(equations_t1x[i]) + "\n";
-		cout << "value" << holder.vals[i] << "\n";
   }
+
+	holder holder;
+	holder.executeEquation(equations_t1, equations_t1x, 25);
+	
+	for(int i=0; i<125; i++){
+    cout << "value " << i % 5 << holder.vals[i] << endl;
+  }
+
   return 0;
+
 }
