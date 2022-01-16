60c157e4ad2351d4886825e1cc5794a5b3ae9c13
Kabir Singh <sahai.kabir1@gmail.com>
2022-01-15 17:28:00 -0700

    shi_bai_zhe

--- linear.reg.new.cpp
+++ linear.reg.new.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 using namespace std;
 
-float real_vals[10];
+float real_vals[11];
 
 void questions(){
 	for(int i=0; i<11; i++){
@@ -12,8 +12,26 @@ void questions(){
 	}
 }
 
+float  return_reg_equation_vals(float m, float b, float reg_vals[11], int iVal){
+	for(int i = 0; i<11; i++){
+		reg_vals[i] = (m * i) + b;
+	}
+	return reg_vals[iVal];
+}
+
+void findEquationDriver(float learning_rate){
+	float m_coefficient = 0;
+	float b_coefficient = 0;
+	float reg_vals[11];
+	for(int i = 0; i<11; i++){
+		float regRunner = return_reg_equation_vals(m_coefficient, b_coefficient, reg_vals, i);
+		cout <<  regRunner << endl;
+		reg_vals[i] = regRunner;
+	}
+}
 
 int main(){
 	questions();
+	findEquationDriver(0.5);
 	return 0;
 }
