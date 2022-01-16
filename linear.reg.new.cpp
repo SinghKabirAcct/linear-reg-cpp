fff532aa75fe8d871d3b10656c69f394b79a4283
Kabir Singh <sahai.kabir1@gmail.com>
2022-01-15 22:15:36 -0700

    whatever

--- linear.reg.new.cpp
+++ linear.reg.new.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 using namespace std;
 
-float real_vals[11];
+double real_vals[11];
 
 void questions(){
 	for(int i=0; i<11; i++){
@@ -12,22 +12,22 @@ void questions(){
 	}
 }
 
-float  return_reg_equation_vals(float m, float b, float reg_vals[11], int iVal){
+double  return_reg_equation_vals(double m, double b, double reg_vals[11], int iVal){
 	for(int i = 0; i<11; i++){
 		reg_vals[i] = (m * i) + b;
 	}
 	return reg_vals[iVal];
 }
 
-float m_coefficient = 0;
-float b_coefficient = 0;
-float errorPrev = numeric_limits<float>::infinity();
+double m_coefficient = 0;
+double b_coefficient = 0.000000000000000;
+double errorPrev = numeric_limits<double>::infinity();
 
-void findEquationDriver(float learning_rate){
-	float reg_vals[11];
-	float errorVal = 0;
+void findEquationDriver(double learning_rate){
+	double reg_vals[11];
+	double errorVal = 0;
 	for(int i = 0; i<11; i++){
-		float regRunner = return_reg_equation_vals(m_coefficient, b_coefficient, reg_vals, i);
+		double regRunner = return_reg_equation_vals(m_coefficient, b_coefficient, reg_vals, i);
 		cout <<  regRunner << endl;
 		reg_vals[i] = regRunner;
 	}
@@ -35,7 +35,7 @@ void findEquationDriver(float learning_rate){
 		errorVal = (reg_vals[i]-real_vals[i]) + errorVal;
 		cout << errorVal << endl;
 	}
-	m_coefficient = m_coefficient - (errorVal * (1/11) * learning_rate);
+	m_coefficient = m_coefficient - (errorVal * learning_rate);
 	if(errorPrev != 0){
 		cout << m_coefficient << endl;
 	}
