748225ff85ff8aab3c7eec5e29e9d258f55bc7b1
Kabir Singh <sahai.kabir1@gmail.com>
2022-01-15 22:10:43 -0700

    i dont like naming things, especially my new child

--- linear.reg.new.cpp
+++ linear.reg.new.cpp
@@ -19,17 +19,28 @@ float  return_reg_equation_vals(float m, float b, float reg_vals[11], int iVal){
 	return reg_vals[iVal];
 }
 
+float m_coefficient = 0;
+float b_coefficient = 0;
+float errorPrev = numeric_limits<float>::infinity();
+
 void findEquationDriver(float learning_rate){
-	float m_coefficient = 0;
-	float b_coefficient = 0;
 	float reg_vals[11];
 	float errorVal = 0;
 	for(int i = 0; i<11; i++){
 		float regRunner = return_reg_equation_vals(m_coefficient, b_coefficient, reg_vals, i);
 		cout <<  regRunner << endl;
 		reg_vals[i] = regRunner;
-		errorVal = ((reg_vals[i]-real_vals[i])*(reg_vals[i]-real_vals[i])) + errorVal;
+	}
+	for(int i = 0; i<11; i++){
+		errorVal = (reg_vals[i]-real_vals[i]) + errorVal;
 		cout << errorVal << endl;
+	}
+	m_coefficient = m_coefficient - (errorVal * (1/11) * learning_rate);
+	if(errorPrev != 0){
+		cout << m_coefficient << endl;
+	}
+	else{
+		cout << m_coefficient << "x + " << b_coefficient << endl;
 	}
 }
 
