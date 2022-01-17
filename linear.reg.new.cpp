3d43d626f5bcbacdcb26a3242c681e0d72dae439
Kabir Singh <sahai.kabir1@gmail.com>
2022-01-16 17:01:17 -0700

    m and b components

--- linear.reg.new.cpp
+++ linear.reg.new.cpp
@@ -21,26 +21,34 @@ double  return_reg_equation_vals(double m, double b, double reg_vals[11], int iV
 
 double m_coefficient = 0;
 double b_coefficient = 0;
-double errorPrev = numeric_limits<double>::infinity();
+double errorPrevM = numeric_limits<double>::infinity();
+double errorPrevB = numeric_limits<double>::infinity();
 double prevm = 0;
+double prevb = 0;
 
 void findEquationDriver(double learning_rate){
 	double reg_vals[11];
-	double errorVal = 0;
+	double errorValM = 0;
+	double errorValB = 0;
 	for(int i = 0; i<11; i++){
 		double regRunner = return_reg_equation_vals(m_coefficient, b_coefficient, reg_vals, i);
-		cout <<  regRunner << endl;
 		reg_vals[i] = regRunner;
 	}
 	for(int i = 0; i<11; i++){
-		errorVal = (reg_vals[i]-real_vals[i]) + errorVal;
-		cout << errorVal << endl;
+		errorValM = ((reg_vals[i]-real_vals[i])*i) + errorValM;
+		errorValB = ((reg_vals[i]-real_vals[i])) + errorValB;
 	}
-	if((abs(errorVal)<=abs(errorPrev)) && abs(errorVal)>0.0001){
-		m_coefficient = m_coefficient - (errorVal * (1.00/11.00) * learning_rate);
-		cout << m_coefficient << endl;
-		errorPrev = errorVal;
+	if((abs(errorValB)>0.00000095) || (abs(errorValM)>0.00000095)){
+		if((abs(errorValM)<=abs(errorPrevM)) && abs(errorValM)>0.00000095){
+			m_coefficient = m_coefficient - (errorValM * (1.00/11.00) * learning_rate);
+		}
+		if((abs(errorValB)<=abs(errorPrevB)) && abs(errorValB)>0.00000095){
+			b_coefficient = b_coefficient - (errorValB * (1.00/11.00) * learning_rate);
+		}
+		errorPrevM = errorValM;
+		errorPrevB = errorValB;
 		prevm = m_coefficient;
+		prevb = b_coefficient;
 		findEquationDriver(0.01);
 	}
 	else{
