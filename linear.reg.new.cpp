bd2acf27a4e2396fde174faac57d36b004196755
Kabir Singh <sahai.kabir1@gmail.com>
2022-01-16 17:19:50 -0700

    more adjustability

--- linear.reg.new.cpp
+++ linear.reg.new.cpp
@@ -4,12 +4,20 @@
 using namespace std;
 
 double real_vals[11];
+double learnRate;
+double accuracyVar;
 
 void questions(){
 	for(int i=0; i<11; i++){
 		cout << "Please Insert the Y Value for X = " << i << ": " << endl;
 		cin >> real_vals[i];
 	}
+	cout << "What learning rate do you want to apply? (reccommended around 0.1)" << endl;
+	cin >> learnRate;
+	cout << "What level of accuracy? (Range: 1-99, reccommended between 90-99)" << endl;
+	cin >> (accuracyVar);
+	accuracyVar = accuracyVar/100000000;
+
 }
 
 double  return_reg_equation_vals(double m, double b, double reg_vals[11], int iVal){
@@ -26,7 +34,7 @@ double errorPrevB = numeric_limits<double>::infinity();
 double prevm = 0;
 double prevb = 0;
 
-void findEquationDriver(double learning_rate){
+void findEquationDriver(double learning_rate, double accVar){
 	double reg_vals[11];
 	double errorValM = 0;
 	double errorValB = 0;
@@ -38,18 +46,18 @@ void findEquationDriver(double learning_rate){
 		errorValM = ((reg_vals[i]-real_vals[i])*i) + errorValM;
 		errorValB = ((reg_vals[i]-real_vals[i])) + errorValB;
 	}
-	if((abs(errorValB)>0.00000095) || (abs(errorValM)>0.00000095)){
-		if((abs(errorValM)<=abs(errorPrevM)) && abs(errorValM)>0.00000095){
+	if((abs(errorValB)>accVar) || (abs(errorValM)>accVar)){
+		if((abs(errorValM)<=abs(errorPrevM)) && abs(errorValM)>accVar){
 			m_coefficient = m_coefficient - (errorValM * (1.00/11.00) * learning_rate);
 		}
-		if((abs(errorValB)<=abs(errorPrevB)) && abs(errorValB)>0.00000095){
+		if((abs(errorValB)<=abs(errorPrevB)) && abs(errorValB)>accVar){
 			b_coefficient = b_coefficient - (errorValB * (1.00/11.00) * learning_rate);
 		}
 		errorPrevM = errorValM;
 		errorPrevB = errorValB;
 		prevm = m_coefficient;
 		prevb = b_coefficient;
-		findEquationDriver(0.01);
+		findEquationDriver(learnRate, accVar);
 	}
 	else{
 		cout << prevm << "x + " << b_coefficient << endl;
@@ -58,6 +66,6 @@ void findEquationDriver(double learning_rate){
 
 int main(){
 	questions();
-	findEquationDriver(0.1);
+	findEquationDriver(learnRate, accuracyVar);
 	return 0;
 }
